54373794ece4fc5b6757c937e86f8c1c722afda5
JeukOh <jeukoh@gmail.com>
2024-05-06 17:05:42 +0900

    feat: respond-to-ping

--- app/main.py
+++ app/main.py
@@ -6,10 +6,17 @@ def main():
     # You can use print statements as follows for debugging, they'll be visible when running tests.
     print("Logs from your program will appear here!")
 
+    PONG = "+PONG\r\n"
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
-    server_socket.accept() # wait for client
-
+    conn, addr = server_socket.accept() # wait for client
+    
+    with conn:
+        data = conn.recv(1024)
+        data = data.decode()
+        # TODO
+        # https://lethain.com/redis-protocol/
+        conn.send(PONG.encode())
 
 if __name__ == "__main__":
     main()
