5e6be66179fa4fc3e7310a99dfd53e5dbb010a97
JeukOh <jeukoh@gmail.com>
2024-05-06 17:27:19 +0900

    feat: Respond to multiple PINGs

--- app/main.py
+++ app/main.py
@@ -9,14 +9,25 @@ def main():
     PONG = "+PONG\r\n"
     # Uncomment this to pass the first stage
     server_socket = socket.create_server(("localhost", 6379), reuse_port=True)
+
+
     conn, addr = server_socket.accept() # wait for client
     
+
     with conn:
-        data = conn.recv(1024)
-        data = data.decode()
-        # TODO
-        # https://lethain.com/redis-protocol/
-        conn.send(PONG.encode())
+        while True:
+            data = conn.recv(1024)
+            if not data:
+                break
+            data = data.decode()
+            # TODO
+            # https://lethain.com/redis-protocol/
+            conn.send(PONG.encode())
+
+
+
+    server_socket.close()
+            
 
 if __name__ == "__main__":
     main()
